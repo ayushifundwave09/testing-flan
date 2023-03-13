7d93eaa0aff659e696534e5219a948a6085ddc23
ayushifundwave09 <ayushi.gupta@getfundwave.com>
2023-03-13 18:58:42 +0530

    adding ecs container file

---
+++ flan-scan/server/report-scanner/ecs_con.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+echo "Creating the cluster with EC2 instance type"
+
+aws ecs create-cluster --cluster-name testing-nmap --region us-east-1
+
+echo "attach the task defination to it"
+
+aws ecs register-task-definition --cli-input-json file://$HOME/testing-flan/flan-scan/server/report-scanner/testing-nmap.json
+
+#aws ecs list-task-definitions
+
+aws ecs run-task --cluster testing-nmap --task-definition testing-nmap:1 --count 1
