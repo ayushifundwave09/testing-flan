d45309b2905da3e75cc692a4a94e6175c000c207
ayushifundwave09 <ayushi.gupta@getfundwave.com>
2023-03-13 19:25:33 +0530

    adding cloudwatch enabled

--- flan-scan/server/report-scanner/ecs_con.sh
+++ flan-scan/server/report-scanner/ecs_con.sh
@@ -3,11 +3,12 @@
 echo "Creating the cluster with EC2 instance type"
 
 aws ecs create-cluster --cluster-name testing-nmap --region us-east-1
+aws ecs update-cluster-settings --cluster testing-nmap --settings name=containerInsights,value=enabled
 
 echo "attach the task defination to it"
 
-aws ecs register-task-definition --cli-input-json file://flan-scan/server/report-scanner/testing-nmap.json
+aws ecs register-task-definition --cli-input-json file://$HOME/testing-flan/flan-scan/server/report-scanner/testing-nmap.json
 
-#aws ecs list-task-definitions
+echo "running the task using the task definaions"
 
 aws ecs run-task --cluster testing-nmap --task-definition testing-nmap:1 --count 1
